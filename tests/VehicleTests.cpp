5c6c8c29af97157fecfcfe59cac6b068f0dd27c2
CrimsonRadiator <michsok@gmail.com>
2018-04-30 17:41:53 +0200

    Example test added.

---
+++ tests/VehicleTests.cpp
@@ -0,0 +1,12 @@
+#define BOOST_TEST_MODULE VehicleTests
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(vehicle_tests_suite)
+
+BOOST_AUTO_TEST_CASE(check_max_vertices_in_vehicle)
+{
+    BOOST_CHECK_EQUAL(1, 1);
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
