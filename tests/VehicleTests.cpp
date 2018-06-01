bc75c699447b761ab07fef47743c8d135e2b608d
CrimsonRadiator <michsok@gmail.com>
2018-06-01 13:14:25 +0200

    Vehicle tests and some World tests.

--- tests/VehicleTests.cpp
+++ tests/VehicleTests.cpp
@@ -1,12 +1,149 @@
 #define BOOST_TEST_MODULE VehicleTests
 #include <boost/test/included/unit_test.hpp>
+#include <Box2D/Box2D.h>
+#include <SFML/Graphics.hpp>
+#include "Vehicle.h"
+#include "World.h"
+#include "Application.h"
+#include "EvolutionaryAlgorithm.h"
+#include <algorithm>
+
+
+//struct VehicleTestFixture{
+//    VehicleTestFixture(int vehicleNumber, int wheelsNumber){
+//        //generate new generation
+//        EvolutionaryAlgorithm algo(vehicleNumber, wheelsNumber, 0.1f);
+//
+//        //fill chromosomes
+//        std::copy(algo.GetCurrentGeneration().begin(),
+//                  algo.GetCurrentGeneration().end(),
+//                  chromosomes.begin());
+//
+//        //fill vehicles
+//        std::copy(chromosomes.begin(),
+//                  chromosomes.end(),
+//                  vehicles.begin());
+//    }
+//
+//    std::vector<Chromosome> chromosomes;
+//    std::vector<Vehicle> vehicles;
+//
+//};
+
+
 
 BOOST_AUTO_TEST_SUITE(vehicle_tests_suite)
 
-BOOST_AUTO_TEST_CASE(check_max_vertices_in_vehicle)
+BOOST_AUTO_TEST_CASE(check_if_deleteBody_deletes_b2Body)
 {
-    BOOST_CHECK_EQUAL(1, 1);
+
+    int count = World::getInstance().getBodyCount();
+    BOOST_CHECK_EQUAL(count, 0);
+    {
+        EvolutionaryAlgorithm algo(20, 2, 0.1f);
+        std::vector<Vehicle> vehicles(algo.GetCurrentGeneration().begin(), algo.GetCurrentGeneration().end());
+        for(auto& v : vehicles)
+            v.deleteBody();
+    }
+    BOOST_CHECK_EQUAL(World::getInstance().getBodyCount(),0);
+}
+
+BOOST_AUTO_TEST_CASE(check_if_chromosome_and_vehicle_have_same_number_of_wheels)
+{
+    for(size_t i=0; i<Chromosome::VERTICES_NUMBER; ++i)
+    {
+        EvolutionaryAlgorithm algo(20, i, 0.1f);
+        std::vector<Chromosome> chromosomes(algo.GetCurrentGeneration().begin(), algo.GetCurrentGeneration().end());
+        std::vector<Vehicle> vehicles(chromosomes.begin(), chromosomes.end());
+        for(size_t j=0; j<vehicles.size(); ++j)
+            BOOST_CHECK_EQUAL(chromosomes[j].WHEELS_NUMBER, vehicles[j].getWheelShapes().size());
+        for(auto& v : vehicles)
+            v.deleteBody();
+    }
 
 }
+
+BOOST_AUTO_TEST_CASE(check_if_chromosome_and_vehicle_have_same_number_of_vertices)
+{
+    EvolutionaryAlgorithm algo(20, 2, 0.1f);
+    std::vector<Chromosome> chromosomes(algo.GetCurrentGeneration().begin(), algo.GetCurrentGeneration().end());
+    std::vector<Vehicle> vehicles(chromosomes.begin(), chromosomes.end());
+    for(size_t i=0; i<vehicles.size(); ++i)
+        BOOST_CHECK_EQUAL(chromosomes[i].VERTICES_NUMBER, vehicles[i].getChassisShape().getPointCount());
+    for(auto& v : vehicles)
+        v.deleteBody();
+}
+
+BOOST_AUTO_TEST_CASE(check_if_vertices_in_chromosome_and_vehicle_are_equal)
+{
+    EvolutionaryAlgorithm algo(20, 2, 0.1f);
+    std::vector<Chromosome> chromosomes(algo.GetCurrentGeneration().begin(), algo.GetCurrentGeneration().end());
+    std::vector<Vehicle> vehicles(chromosomes.begin(), chromosomes.end());
+
+    auto chromosome = chromosomes.begin();
+    auto vehicle = vehicles.begin();
+    for(;chromosome!=chromosomes.end();++chromosome, ++vehicle) {
+
+        std::vector<std::pair<float, float>> chassisVertices;
+        for (size_t i = 0; i < vehicle->getChassisShape().getPointCount(); ++i) {
+            chassisVertices.emplace_back(
+                    vehicle->getChassisShape().getPoint(i).x / SCALE,
+                    vehicle->getChassisShape().getPoint(i).y / SCALE);
+        }
+
+        BOOST_CHECK_EQUAL(std::is_permutation(chassisVertices.begin(),
+                                              chassisVertices.end(),
+                                              chromosome->getBodyVertices().begin(),
+                                              [](const std::pair<float, float> &a,
+                                                 const std::pair<float, float> &b) -> bool {
+
+                                                  return std::abs(a.first - b.first) < 0.01 &&
+                                                         std::abs(a.second - b.second) < 0.01;
+                                              }), true);
+    }
+    for(auto& v : vehicles)
+        v.deleteBody();
+}
+
+BOOST_AUTO_TEST_CASE(check_if_wheel_sizes_in_chromosome_and_vehicle_are_equal)
+{
+    EvolutionaryAlgorithm algo(20, 2, 0.1f);
+    std::vector<Chromosome> chromosomes(algo.GetCurrentGeneration().begin(), algo.GetCurrentGeneration().end());
+    std::vector<Vehicle> vehicles(chromosomes.begin(), chromosomes.end());
+    auto chromosome = chromosomes.begin();
+    auto vehicle = vehicles.begin();
+    for(;chromosome!=chromosomes.end();++chromosome, ++vehicle) {
+        std::vector<float> wheels;
+        for(auto& wheel : vehicle->getWheelShapes())
+            wheels.emplace_back(wheel.getRadius()/SCALE);
+
+         BOOST_CHECK_EQUAL(std::is_permutation(chromosome->getWheelSizes().begin(),
+                                              chromosome->getWheelSizes().end(),
+                                              wheels.begin(),
+                                              [](const float &a,
+                                                 const float &b) -> bool {
+
+                                                  return std::abs(a- b) < 0.01;
+                                              }), true);
+    }
+    for(auto& v : vehicles)
+        v.deleteBody();
+
+
+}
+
+BOOST_AUTO_TEST_CASE(check_if_world_deletes_body)
+{
+    int bodyCounter = World::getInstance().getBodyCount();
+    BOOST_CHECK_EQUAL(0, bodyCounter);
+    auto dynamicBody = World::getInstance().createDynamicBody(0, 0);
+    auto staticBody = World::getInstance().createStaticBody(10, 10);
+    BOOST_CHECK_EQUAL(World::getInstance().getBodyCount(), 2);
+    World::getInstance().destroyBody(dynamicBody);
+    World::getInstance().destroyBody(staticBody);
+    BOOST_CHECK_EQUAL(World::getInstance().getBodyCount(), 0);
+
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
